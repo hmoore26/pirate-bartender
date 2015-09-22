52c85e602baec95fa9989ea08bdd06a7b0d49f6f
Harold Moore <haroldmmoore@yahoo.com>
2015-09-22 22:35:38 +0000

    Apply notes for future changes. Prepare code for future changes.

--- pirate_bartender.py
+++ pirate_bartender.py
@@ -1,9 +1,17 @@
+"""Module-level docstring; which describe the module's purpose,
+as well as elaborating on constants, notes, usage.
+
+Tip:
+    random.choice(): --
+
+"""
+
 import random
 
-answers = {}
 
 # These are the type of drink and follow up question
-
+# this is a constant
+# questions (rename): QUESTIONS
 questions = {
     "strong": "Do ye like yer drinks strong?",
     "salty": "Do ye like it with a salty tang?",
@@ -13,7 +21,6 @@ questions = {
 }
 
 # These are the corresponding ingredients
-
 ingredients = {
     "strong": ["glug of rum", "slug of whisky", "splash of gin"],
     "salty": ["olive on a stick", "salt-dusted rim", "rasher of bacon"],
@@ -23,30 +30,52 @@ ingredients = {
 }
 
 # Random drink names
-
 drinkname = ["Fuzzy Navel", "Bridge Troll", "Death Door", "Ring of fire"]
 
-def ask_questions():
-    print "Answer each question ('y' or 'n') to determine your drink."
+
+def ask_questions(answers):
+    answers = {}
+    print("Answer each question ('y' or 'n') to determine your drink.")
+
     for key, value in questions.items():
         reply = raw_input(value).lower()
+
         if reply == "y":
             answers[key] = True
         else:
             answers[key] = False
+
     return answers
 
-#Create the drink based on the answers given by matching answers and selecting a random matching ingredient.Then choose a random name from drinkname list
-   
+
 def create_drink(answers, ingredients):
+    """Create the drink based on the answers given by matching answers
+    and selecting a random matching ingredient. Then choose a random
+    name from the drinkname list.
+
+    Args:
+        answer (dict): --
+        ingredients (dicts): --
+
+    Returns:
+        str: The name of the new drink.
+
+    """
    
+    print(a)
     print "\nHere's your drink! It is a {drinkname}.".format(drinkname=random.choice(drinkname))
+
     for key, value in answers.items():
+
         if value is True:
-            print "{0}".format(ingredients[key][random.randint(0, 2)])
-    return
+            # use random.choice instead
+            new_drink ="{0}".format(ingredients[key][random.randint(0, 2)])
+
+    return new_drink
 
 
+# Runtime -- local scope, this doesn't execute if this module
+# is imported, rather than ran through python directly.
 if __name__ == "__main__":
-    ask_questions()
-    create_drink(answers,ingredients)
+    answers = ask_questions()
+    create_drink(answers, ingredients)
