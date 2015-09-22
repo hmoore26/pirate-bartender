913cee10ecbbe9f6f4f891b68536d89be960ccee
hmoore26 <hmoore26@users.noreply.github.com>
2015-09-22 10:39:45 -0400

    Final version

--- pirate_bartender.py
+++ pirate_bartender.py
@@ -1,28 +1,52 @@
-def find_preferences():
-    preferences = {}
-    for type, question in questions.iteritems():
-        print question
-        preferences[type] = raw_input().lower() in ["y", "yes"]
-        print ""
-    return preferences
-    
-def make_drink(preferences):
-    drink = []
-    for ingredient_type, liked in preferences.iteritems():
-        if not liked:
-            continue
+import random
 
-        drink.append(random.choice(ingredients[ingredient_type]))
-    return drink
+answers = {}
 
+# These are the type of drink and follow up question
 
-def main():
-    preferences = find_preferences()
-    drink = make_drink(preferences)
-    print "One drink coming up."
-    print "It's full of good stuff.  The recipe is:"
-    for ingredient in drink:
-        print "A {}".format(ingredient)
+questions = {
+    "strong": "Do ye like yer drinks strong?",
+    "salty": "Do ye like it with a salty tang?",
+    "bitter": "Are ye a lubber who likes it bitter?",
+    "sweet": "Would ye like a bit of sweetness with yer poison?",
+    "fruity": "Are ye one for a fruity finish?"
+}
+
+# These are the corresponding ingredients
+
+ingredients = {
+    "strong": ["glug of rum", "slug of whisky", "splash of gin"],
+    "salty": ["olive on a stick", "salt-dusted rim", "rasher of bacon"],
+    "bitter": ["shake of bitters", "splash of tonic", "twist of lemon peel"],
+    "sweet": ["sugar cube", "spoonful of honey", "spash of cola"],
+    "fruity": ["slice of orange", "dash of cassis", "cherry on top"]
+}
+
+# Random drink names
+
+drinkname = ["Fuzzy Navel", "Bridge Troll", "Death Door", "Ring of fire"]
+
+def ask_questions():
+    print "Answer each question ('y' or 'n') to determine your drink."
+    for key, value in questions.items():
+        reply = raw_input(value).lower()
+        if reply == "y":
+            answers[key] = True
+        else:
+            answers[key] = False
+    return answers
+
+#Create the drink based on the answers given by matching answers and selecting a random matching ingredient.Then choose a random name from drinkname list
+   
+def create_drink(answers, ingredients):
+   
+    print "\nHere's your drink! It is a {drinkname}.".format(drinkname=random.choice(drinkname))
+    for key, value in answers.items():
+        if value is True:
+            print "{0}".format(ingredients[key][random.randint(0, 2)])
+    return
+
 
 if __name__ == "__main__":
-    main()
+    ask_questions()
+    create_drink(answers,ingredients)
